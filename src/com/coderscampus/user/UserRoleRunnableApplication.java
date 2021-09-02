075dfdafcaa7e0df72716b650d09daf05b096459
mkfah809 <minakaramm1@gmail.com>
2021-09-01 20:19:21 -0400

    Assign4_V2

--- src/com/coderscampus/user/UserRoleRunnableApplication.java
+++ src/com/coderscampus/user/UserRoleRunnableApplication.java
@@ -2,14 +2,28 @@ package com.coderscampus.user;
 
 import java.io.IOException;
 
+<<<<<<< Updated upstream
 import com.coderscampus.userfunctions.UserFromFile;
+=======
+import com.coderscampus.fileinformation.FileServiceImpl;
+
+>>>>>>> Stashed changes
 
 public class UserRoleRunnableApplication {
 
 	public static void main(String[] args) throws IOException {
+<<<<<<< Updated upstream
 
 		UserFromFile userfromFile = new UserFromFile();
 		System.out.println(userfromFile.getUserInformationFromFile("UserData.txt"));
+=======
+		
+		FileServiceImpl fileservice = new FileServiceImpl();
+		fileservice.getUserInformationFromFile("UserData.txt");
+		
+		User superuser = new SuperUser();
+		 
+>>>>>>> Stashed changes
 	
 	}
 
--- src/com/coderscampus/userfunctions/UserLogin.java
+++ src/com/coderscampus/userfunctions/UserLogin.java
@@ -4,23 +4,43 @@ import java.util.Scanner;
 
 import com.coderscampus.user.SuperUserInterface;
 
+<<<<<<< Updated upstream
 public class UserLogin implements SuperUserInterface {
 
 	@Override
 	public String getUserLogin(String username, String password) {
+=======
+public class  UserLogin implements SuperUserInterface {
+
+	@Override
+	public String getUserLogin(String username, String password) {
+		@SuppressWarnings("resource")
+>>>>>>> Stashed changes
 		Scanner login = new Scanner(System.in);
 		String userLogin = login.nextLine();
 		return userLogin;
+		
 	}
 
 	@Override
+<<<<<<< Updated upstream
 	public String updateUserInformationToFile(String oldInput) {
 		// TODO Auto-generated method stub
 		return null;
+=======
+	public void updateUserInformationToFile() {
+		// TODO Auto-generated method stub
+		
+>>>>>>> Stashed changes
 	}
 
 
 
 
 
+<<<<<<< Updated upstream
+=======
+
+
+>>>>>>> Stashed changes
 }
