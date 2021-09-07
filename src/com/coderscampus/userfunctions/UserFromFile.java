16d303d240ab13bacd81bdcfcdb7e386290538a6
mkfah809 <minakaramm1@gmail.com>
2021-09-07 17:57:01 -0400

    V2_Assignment4
    Demonstrates retrieving user from a file.

--- src/com/coderscampus/userfunctions/UserFromFile.java
+++ src/com/coderscampus/userfunctions/UserFromFile.java
@@ -1,44 +1,78 @@
 package com.coderscampus.userfunctions;
 
 import java.io.*;
+
 import com.coderscampus.user.User;
 
 public class UserFromFile {
 
-	private BufferedReader reader = null;
 	private static String line = null;
 	private static String[] userinfoArray;
+	private static String userUsername = "";
+	private static String userpassword = "";
+	static User[] users = new User[20];
+	private static int count = 0;
+	public static int test = 0;
+	@SuppressWarnings("unused")
+	private static boolean userFound = false;
 
-	public String getUserInformationFromFile(String fileName) throws IOException {
-		try {
-			reader = new BufferedReader(new FileReader(fileName));
-			getuserInformation();
+	public static String getuserInformation() throws IOException {
+		getuserreadInfo();
+		do {
+			getpromptUser();
+			getuser();
+		} while (test == 4);
+		return line;
+	}
 
-		} finally {
-			if (reader != null) {
-				reader.close();
+	private static void getuser() {
+		String welcomeUser = "";
+		//HERE IS THE BUG!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+		for (User user : users) {
+			if (userUsername.equals(user.getUsername()) && userpassword.equals(user.getPassword())) {
+				userFound = true;
+				welcomeUser = user.getName();
+				System.out.println("welcome: " + welcomeUser);
+			} else {
+				System.out.println("Wrong Entry");
+				System.exit(0);
 			}
 		}
-		return null;
 	}
 
-	private String getuserInformation() throws IOException {
-
-		while ((line = reader.readLine()) != null) {
-			//System.out.println(line);
-			User user = new User();
-			userinfoArray = line.split(",");
-			user.setUsername(userinfoArray[0]);
-			user.setPassword(userinfoArray[1]);
-			user.setName(userinfoArray[2]);
-			user.setRole(userinfoArray[3]);
-			UserLogin userlogin = new UserLogin();
-			userlogin.getUserLogin("Enter your username: ", user.getName());
-			userlogin.getUserLogin("Enter your password: ", user.getPassword());
-
+	@SuppressWarnings("resource")
+	public static void getuserreadInfo() throws IOException {
+		BufferedReader fileReader = null;
+		try {
+			fileReader = new BufferedReader(new FileReader("UserData.txt"));
+			while ((line = fileReader.readLine()) != null) {
+				System.out.println(line);
+				User user = new User();
+				userinfoArray = line.split(",");
+				user.setUsername(userinfoArray[0]);
+				user.setPassword(userinfoArray[1]);
+				user.setName(userinfoArray[2]);
+				user.setRole(userinfoArray[3]);
+				users[count] = user;
+				count++;
+				if (count == 20) {
+					break;
+				}
+			}
+		} finally {
+//			if (fileReader != null) {
+			// fileReader.close();
 		}
-		return line;
-
 	}
 
+	
+
+	@SuppressWarnings("unused")
+	private static void getpromptUser() {
+
+		System.out.println("Enter your Username here: ");
+		userUsername = UserLogin.userInput();
+		System.out.println("Enter your Password here: ");
+		userpassword = UserLogin.userInput();
+	}
 }
--- src/com/coderscampus/userfunctions/UserLogin.java
+++ src/com/coderscampus/userfunctions/UserLogin.java
@@ -2,14 +2,31 @@ package com.coderscampus.userfunctions;
 
 import java.util.Scanner;
 
-public class  UserLogin  {
+public class UserLogin {
+//	@SuppressWarnings("unused")
+//	private static String userUsername;
+//	@SuppressWarnings("unused")
+//	private static String userpassword;
+//
+//	private static String getUserLogin(String promptMsg) {
+//		@SuppressWarnings("resource")
+//		Scanner login = new Scanner(System.in);
+//		System.out.println(promptMsg);
+//		String userLogin = login.nextLine();
+//		return userLogin;
+//	}
+//
+//	public static void getpromptUser() {
+//		userUsername = getUserLogin("Enter your username: ");
+//		userpassword = getUserLogin("Enter your password: ");
+//	}
+	@SuppressWarnings("resource")
+	public static String userInput() {
 
-	
-	public String getUserLogin(String username, String password) {
-		@SuppressWarnings("resource")
-		Scanner login = new Scanner(System.in);
-		String userLogin = login.nextLine();
+		Scanner userisvalidInput = new Scanner(System.in);
+		String userLogin = userisvalidInput.nextLine();
 		return userLogin;
-		
 	}
+	
+	
 }
